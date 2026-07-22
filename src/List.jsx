401de6bf7534c58ed91cc34c0fef09bcc6eac5b4
Simon Luu <simonluu@Simons-MacBook-Pro-13.local>
2026-07-21 20:56:28 -0400

    module 3 again

--- src/List.jsx
+++ src/List.jsx
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 
-const produce = [
-    { name: "Apple", type: "Fruit" },
-    { name: "Banana", type: "Fruit" },
-    { name: "Carrot", type: "Vegetable" },
-    { name: "Broccoli", type: "Vegetable" },
-    { name: "Orange", type: "Fruit" },
-    { name: "Spinach", type: "Vegetable" }
-];
-
 class Module3List extends Component {
     render () {
         return (
             <div className="module">
                 <h1><u>Module 3</u></h1>
-                
+                <h2>This is where I would add a List I guess</h2>
             </div>
         );
     }
