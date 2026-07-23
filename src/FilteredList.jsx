a59fd1cdf9eb8d34f6d4b8125da01cbd74dc5876
Simon Luu <simonluu@Simons-MacBook-Pro-13.local>
2026-07-22 23:52:42 -0400

    final commit

--- src/FilteredList.jsx
+++ src/FilteredList.jsx
@@ -4,26 +4,7 @@ class FilteredList extends Component {
     render() {
         return (
             <div className="module">
-                this.state = {
-                    search: "",
-                    type: "All"
-                };
-                onFilter = (eventKey) => {
-                    this.setState({type: eventKey});
-                };
-                filterItem = (item) => {
-                    const matchesSearch = item.name
-                    .toLowerCase()
-                    .search(this.state.search) !== -1;
-                    const matchesType = this.state.type === "All" 
-                    || item.type === this.state.type;
-                    return matchesSearch && matchesType;
-                }
-                <DropdownButton title="Filter" onSelect={this.onFilter}>
-                    <MenuItem eventKey="All">All</MenuItem>
-                    <MenuItem eventKey="Fruit">Fruit</MenuItem>
-                    <MenuItem eventKey="Vegetable">Vegetables</MenuItem>
-                </DropdownButton>
+                <h6>couldn't get it to work</h6>
             </div>
         );
     }
